0e99c50f318ae09edd7589d841299853d9ec1013
Sunny <thstkd92@gmail.com>
2021-06-15 22:45:16 +0900

    entity Account 추가

---
+++ src/main/java/com/example/springoauth2client/model/Account.java
@@ -0,0 +1,31 @@
+package com.example.springoauth2client.model;
+
+import lombok.Getter;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Getter
+@ToString
+@Entity
+public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+    private String email;
+
+    protected Account() {
+    }
+
+    public Account(Long id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+}
