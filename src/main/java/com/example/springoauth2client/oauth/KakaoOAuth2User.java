6ca83dbb8a1890e5c7aace9d1a33d1aba9b60004
Sunny <thstkd92@gmail.com>
2021-06-17 00:03:19 +0900

    KakaoOAuth2User OAuthAttributes 를 통하여 정보를 받아오려 했으나 null 이 들어왔음

--- src/main/java/com/example/springoauth2client/oauth/KakaoOAuth2User.java
+++ src/main/java/com/example/springoauth2client/oauth/KakaoOAuth2User.java
@@ -1,26 +1,29 @@
 package com.example.springoauth2client.oauth;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Builder;
 import lombok.Getter;
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @Getter
 public class KakaoOAuth2User implements OAuth2User {
 
-    private List<GrantedAuthority> authorities = AuthorityUtils.createAuthorityList("ROLE_USER");
-    private String id;
+    private final String id;
+    private final Set<SimpleGrantedAuthority> authorities;
+    private final Map<String, Object> kakaoAccount;
+    private final Map<String, Object> properties;
 
-    @JsonProperty("kakao_account")
-    private Map<String, Object> kakaoAccount = new HashMap<>();
-
-    private Map<String, Object> properties = new HashMap<>();
+    @Builder
+    public KakaoOAuth2User(String id, Set<SimpleGrantedAuthority> authorities,
+                           Map<String, Object> kakaoAccount, Map<String, Object> properties) {
+        this.id = id;
+        this.authorities = authorities;
+        this.kakaoAccount = kakaoAccount;
+        this.properties = properties;
+    }
 
     @Override
     public Map<String, Object> getAttributes() {
@@ -38,5 +41,16 @@ public class KakaoOAuth2User implements OAuth2User {
     @Override
     public String getName() {
         return this.id;
+    }
+
+    public static KakaoOAuth2User from(OAuthAttributes oAuthAttributes) {
+        Set<SimpleGrantedAuthority> authorities = Collections.singleton(new SimpleGrantedAuthority("ROLE_USER"));
+        Map<String, Object> attributes = oAuthAttributes.getAttributes();
+        return KakaoOAuth2User.builder()
+                .id(oAuthAttributes.getId())
+                .kakaoAccount((Map<String, Object>) attributes.get("kakao_account"))
+                .kakaoAccount((Map<String, Object>) attributes.get("properteis"))
+                .authorities(authorities)
+                .build();
     }
 }
